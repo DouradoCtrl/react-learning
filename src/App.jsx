ee0bfa6400a69391749a11f855dba624be0eb725
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-06 11:37:23 -0300

    add: criando componente com interação state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 function App() {
-  const message = "Olá, mundo!";
+  // State\
+  const [message, setMessage] = useState("Olá, mundo"); // Hook
+
   return (
     <div>
       <h1>{message}</h1>
+      <button
+        onClick={function () {
+          setMessage("Olá, fui clicado!");
+        }}
+      >
+        Mudar mensagem
+      </button>
     </div>
   );
 }
