608f2df7162f391de209d938fcd4be0a5ae22b14
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-06 14:57:49 -0300

    add: criado estrutura de componente de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,65 @@
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
+import { useState } from "react";
+// componente pai
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      description: "Estudar React para aprender a criar aplicações web",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Fazer exercícios de React",
+      description: "Fazer exercícios de React para praticar o que aprendi",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Criar um projeto com React",
+      description:
+        "Criar um projeto com React para colocar em prática o que aprendi",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(id) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(id) {
+    //vai ter todas as tarefas, diferente desse id
+    const newTasks = tasks.filter((task) => task.id !== id);
+    setTasks(newTasks);
+  }
+
   return (
     <div>
-      <h1 className="text-red-600">Gereciador de Tarefas</h1>
-      <AddTask />
-      <Tasks />
+      <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+        <div className="w-[500px]">
+          <h1 className="text-3xl text-slate-100 font-bold text-center">
+            Gereciador de Tarefas
+          </h1>
+          <AddTask />
+          <Tasks
+            tasks={tasks}
+            onTaskClick={onTaskClick}
+            onDeleteTaskClick={onDeleteTaskClick}
+          />
+          {/* componente filho */}
+        </div>
+      </div>
     </div>
   );
 }
