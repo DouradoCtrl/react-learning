52185c45dd7b2844ab970cce7b081b81dccef9e5
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-06 15:55:29 -0300

    edit: desestruturando as props

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,14 +1,12 @@
 import { ChevronRightIcon, Trash2Icon } from "lucide-react";
 
-function Tasks(props) {
-  console.log(props);
-
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((task) => (
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
-            onClick={() => props.onTaskClick(task.id)}
+            onClick={() => onTaskClick(task.id)}
             className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted && "line-through bg-green-600"}`}
           >
             {task.title}
@@ -17,7 +15,7 @@ function Tasks(props) {
             <ChevronRightIcon />
           </button>
           <button
-            onClick={() => props.onDeleteTaskClick(task.id)}
+            onClick={() => onDeleteTaskClick(task.id)}
             className="bg-red-400 text-white p-2 rounded-md"
           >
             <Trash2Icon />
