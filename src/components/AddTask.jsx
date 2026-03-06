573bf6ae01ede89738bf3be882531ad1ac57df75
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-06 15:54:23 -0300

    add: create component for add new task

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,3 +1,44 @@
-function AddTask() {}
+import { useState } from "react";
+
+function AddTask({ onAddTaskSubmit }) {
+  //destructuring props
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <input
+        className="p-2 rounded-md outline-slate-400"
+        type="text"
+        placeholder="Digite o título da tarefa"
+        value={title}
+        onChange={() => setTitle(event.target.value)}
+      ></input>
+      <input
+        className="p-2 rounded-md outline-slate-400"
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        value={description}
+        onChange={() => setDescription(event.target.value)}
+      ></input>
+      <button
+        onClick={() => {
+          if (!title.trim() || !description.trim()) {
+            return alert(
+              "Por favor, preencha o título e a descrição da tarefa",
+            );
+          }
+
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+        className="font-bold bg-slate-500 hover:bg-slate-700 p-2 rounded-md text-white"
+      >
+        Adicionar
+      </button>
+    </div>
+  );
+}
 
 export default AddTask;
