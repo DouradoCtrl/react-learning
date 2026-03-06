1de7284b83af11bf5ed7dfee9233ade6d0760cd8
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-06 16:58:09 -0300

    add: criado rotas para redirecionamento

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import TasksPage from "./pages/TaskPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/task",
+    element: <TasksPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
 // Responsável por inserir o conteúdo
