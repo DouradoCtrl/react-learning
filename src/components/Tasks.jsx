687a374d4ca558455d06389e797549ebfa2c34f7
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-06 14:58:43 -0300

    add: criado interações em State de tarefa concluída e excluir terefa

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,31 @@
-function Tasks() {
-  return <h1>Tasks</h1>;
+import { ChevronRightIcon, Trash2Icon } from "lucide-react";
+
+function Tasks(props) {
+  console.log(props);
+
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted && "line-through bg-green-600"}`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 text-white p-2 rounded-md">
+            <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => props.onDeleteTaskClick(task.id)}
+            className="bg-red-400 text-white p-2 rounded-md"
+          >
+            <Trash2Icon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;
